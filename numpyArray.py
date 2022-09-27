e81a9219c2468d18925552cc6e95840bb871777e
Vincent Medrano <90278614+VincentMed@users.noreply.github.com>
2022-09-26 18:28:46 -0700

    Update numpyArray.py

--- numpyArray.py
+++ numpyArray.py
@@ -23,27 +23,27 @@ Use slicing to produce and print:
 import numpy as np
 
 # create an array with 3 rows and 7 columns.
-myArray = np.arange(21).reshape(3,7)
+A = np.arange(21).reshape(3,7)
 
 # test
 print(np.arange(21).reshape(3,7))
 
 # row 2 of A
-row_two = myArray[1]
+row_two = A[1]
 print(row_two)
 
 # print the above reversed
-rev = myArray[1]
+rev = A[1]
 print(rev[::-1])
 
 # sub matrix, bottom right 2x2
-two_by_two = myArray[1, 5:7], myArray[2, 5:7]
+two_by_two = A[1, 5:7], A[2, 5:7]
 print(two_by_two)
 
 # bottom left 2x2 array
-bottom_left_array = myArray[1, 0:2], myArray[2, 0:2]
+bottom_left_array = A[1, 0:2], A[2, 0:2]
 print(bottom_left_array)
 
 #  3x3 sub-array in the middle of A
-three_by_three = myArray[0, 2:5], myArray[1, 2:5], myArray[2, 2:5]
+three_by_three = A[0, 2:5], A[1, 2:5], A[2, 2:5]
 print(three_by_three)
